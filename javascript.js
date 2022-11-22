eaa629d13c5e629800b345d3480bf36ebb075e08
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-11-22 22:59:05 +0100

    Add getPlayerSelection JS function, update pseudocode

--- javascript.js
+++ javascript.js
@@ -12,7 +12,8 @@ function getRandomInt(min, max) {
 function playRound(playerSelection, computerSelection) {
   /*
     ask the player to make a selection
-    read the selection and assign it to a variable
+    read the selection and verify if it's valid
+    if valid, assign it to a variable
     make the selection's format match computerSelection format
 
     getComputerChoice and assign it to a variable
@@ -21,4 +22,20 @@ function playRound(playerSelection, computerSelection) {
 
     display a lose/win message to the player
     */
+}
+
+function getPlayerSelection() {
+  const validOptions = ["rock", "paper", "scissors"];
+
+  const playerInput = prompt(
+    "Choose one: Rock, Paper, or Scissors."
+  ).toLowerCase();
+
+  if (!validOptions.includes(playerInput)) {
+    console.warning(`Invalid option: "${playerInput}". Try again.`);
+    getPlayerSelection();
+  }
+
+  //Return playerInput formatted as: Rock / Paper / Scissors
+  return playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
 }
