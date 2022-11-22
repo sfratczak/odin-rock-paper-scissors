06175f8c4431a18da33714245deac16a8d8dfa68
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-11-22 22:28:30 +0100

    Add playRound JS function with pseudocode

--- javascript.js
+++ javascript.js
@@ -8,3 +8,17 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+function playRound(playerSelection, computerSelection) {
+  /*
+    ask the player to make a selection
+    read the selection and assign it to a variable
+    make the selection's format match computerSelection format
+
+    getComputerChoice and assign it to a variable
+    
+    compare playerselection with computerselection and choose a winner
+
+    display a lose/win message to the player
+    */
+}
