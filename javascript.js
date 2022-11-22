73a3183921c8b41ecf180181d536dcb5effddaaa
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-11-22 23:46:14 +0100

    Add note about new recursion bug in getPlayerChoice

--- javascript.js
+++ javascript.js
@@ -47,11 +47,15 @@ function getPlayerChoice() {
     "Choose one: Rock, Paper, or Scissors."
   ).toLowerCase();
 
+  //TODO: Bug
+  // if user provides an invalid input in first function call and then
+  // provides a valid one in the second function call (recursion), the function
+  // still returns the invalid one.
   if (!validOptions.includes(playerInput)) {
     console.warn(`Invalid option: "${playerInput}". Try again.`);
     getPlayerChoice();
-  } else {
-    //Return playerInput formatted as: Rock / Paper / Scissors
-    return playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
   }
+
+  //Return playerInput formatted as: Rock / Paper / Scissors
+  return playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
 }
